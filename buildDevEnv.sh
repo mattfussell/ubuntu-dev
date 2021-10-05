d90df7cba8f0ce43b5da47859ad45f58f308ad74
Matt Fussell <matt.fussell@gmail.com>
2021-10-04 21:55:18 -0500

    Fixes and updates
    This is why we don't commit in a hurry. 
    Package update 'nvm' > 'npm'.
    VSCode apt source added and install process updated
    Additional packages added to cover gaps in LXLE - refinement to happen later.

--- buildDevEnv.sh
+++ buildDevEnv.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-sudo apt-get install aptitude localepurge nodejs nvm git gitsome code
+# There aren't any checks written into this file yet - it will be refined over time.
+# It is currently a very ham-fisted way to get everything in place as quickly as possible.
+
+# Get the VSCode repos into the list of software sources
+# 04-10-2021: https://code.visualstudio.com/docs/setup/linux
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+# Install the packages for node development
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install aptitude firefox localepurge nodejs npm git gitsome code
