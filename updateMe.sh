19695a31b1a01bd09f833bea2cfaf0515bf09a25
Matt Fussell <matt.fussell@gmail.com>
2021-10-04 21:58:49 -0500

    updated the update file

--- updateMe.sh
+++ updateMe.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-#update the VM
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoclean -y && sudo apt-get autoremove -y
+# update the VM - uncomment if running the inaugural update
+# sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoclean -y && sudo apt-get autoremove -y
+
+# update the VM - uncomment if all the build software has been installed
+sudo apt-get update
+sudo aptitude safe-upgrade -y
+sudo apt-get autoclean -y
+sudo apt-get autoremove -y
